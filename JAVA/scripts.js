754db5281786fcc09e25781fe517cbf5195ba654
PSDRI <142747634+PSDRI@users.noreply.github.com>
2023-12-04 22:37:06 -0300

    site novo

---
+++ JAVA/scripts.js
@@ -0,0 +1,20 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetPosition = document.querySelector(targetId).offsetTop;
+
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth'
+        });
+    }
+});
